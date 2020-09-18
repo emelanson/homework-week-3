7b376830d41987a26704479dab280f9799a44929
emelanson <elijahmelanson@gmail.com>
2020-09-18 01:07:37 -0400

    Began outlining the init procedure to take user variables and evaluate them

--- script.js
+++ script.js
@@ -32,13 +32,10 @@ generateBtn.addEventListener("click", writePassword);
 //Lower: 97-122
 
 //Could just make a function with randomInt(2), if 1 then .toUpper()
-
-
-
 // use toUpper() when needed
 //var letterArr = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
 
-// could also try math.random()*9 or getRandomInt().  use .toString() later
+// could also try math.random()*9 or getRandomInt().  use .toString() later.  
 //var numArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 //Special characters array.  Might be a better way?
@@ -52,19 +49,57 @@ function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min) ) + min;
 }
 
-//here is the method I can use to create ASCII characters from the random number generator
-var a = String.fromCharCode(getRndInteger(33, 127))
+//create ASCII characters from the random number generator
+//String.fromCharCode(getRndInteger(33, 127))
 
-function generatePassword(passLength, acceptSpecial, acceptUpper, acceptLower, acceptNums) {
-  
-  //our end condition for this code block is generating a single character to add to our password.
-  //if we return a string, we know we have converted the character and it is ready to add to our password.
-//   while (typeof(numForCharacter !== 'string')); {
-//     var numForCharacter = getRndInteger(33, 127);
+//A universal function to evaluate whether we should reject or accept a character based on our variable
 
-//     //Case statements for our different ranges
-//     if (numForCharacter) {
-//     }
+// function acceptanceCheck(acceptType);{
+//   if (acceptType){
+//     return true;
+//   } else{
 
+//   }
 
 // }
+
+//Prompts for our Password gen Variables
+//password needs to be between 8 and 128 characters
+//we need to check to make sure we have at least one acceptance criterea turned on
+
+function initialPrompts();{
+
+}
+
+
+function generatePassword(passLength, acceptSpecial, acceptUpper, acceptLower, acceptNums) {
+
+  var storeChar = "";
+  console.log("storeChar", storeChar);
+
+  //our end condition for this code block is generating a single character to add to our password.
+  //if we return a string, we know we have converted the character and it is ready to add to our password.
+   while (typeof(numForCharacter !== 'string')); {
+     var numForCharacter = getRndInteger(33, 127);
+     console.log("numForCharacter", numForCharacter);
+
+     //Case statements for our different ranges.  
+     //Find range of number
+     //then check if we accept that type of character based on lookup table
+     //reject the number if it isn't an accepted character
+     //since specials are a disjointed set, leave their case till last.
+
+     //Check if number
+     if (numForCharacter >=48 && numForCharacter < 58);{
+       if (acceptNums){
+         storeChar += String.fromCharCode(numForCharacter)
+       } else {
+         return
+       }
+
+     }
+
+    }
+
+
+ }
